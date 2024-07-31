3c8108d6b9b51f35eed08c3ce4eafefba13b6def
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2024-07-31 14:08:24 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const smallImgs = document.querySelectorAll('.list-item');
+const largeImg = document.querySelector('.gallery__large-img');
+
+smallImgs.forEach((image) => {
+  image.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const link = image.querySelector('.list-item__link');
+
+    largeImg.src = link.href;
+  });
+});
