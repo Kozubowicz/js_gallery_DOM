a26f4a428b37c1a6ee63adf48680117564f7510e
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2024-07-31 14:19:45 +0200

    make build

---
+++ index.d8e43b73.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll(".list-item"),r=document.querySelector(".gallery__large-img");e.forEach(function(e){e.addEventListener("click",function(t){t.preventDefault();var l=e.querySelector(".list-item__link");r.src=l.href})});
+//# sourceMappingURL=index.d8e43b73.js.map
